bb8396017fafd623dad38dd66cb49f8b8840ac0f
prado <pradojimenezmaria@gmail.com>
2023-11-18 03:04:02 -0600

    Se agrego header

---
+++ BienesRaices/resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header class="header inicio header inicio" style="background-image: url({{Vite::asset('resources/img/header.jpg')}})"">
+    <div class="contenedor contenido-header">
+        <div class="barra">
+            <a href="index.html">
+                <img src="{{Vite::asset('resources/img/logo.svg')}}" alt="Logotipo de Bienes Raices">
+            </a>
+
+            <div class="mobile-menu">
+                <img src="{{Vite::asset('resources/img/barras.svg')}}" alt="icono menu responsive">
+            </div>
+
+            <div class="derecha">
+                <img class="dark-mode-boton" src="{{Vite::asset('resources/img/dark-mode.svg')}}">
+                <nav class="navegacion">
+                    <a href="nosotros.html">Nosotros</a>
+                    <a href="anuncios.html">Anuncios</a>
+                    <a href="blog.html">Blog</a>
+                    <a href="contacto.html">Contacto</a>
+                </nav>
+            </div>
+
+            
+        </div> <!--.barra-->
+
+        <h1>Venta de Casas y Departamentos  Exclusivos de Lujo</h1>
+    </div>
+</header><!--Fin del Header-->
