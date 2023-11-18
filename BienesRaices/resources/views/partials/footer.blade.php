c51addf9f913b026c5d43b551ee2f7be0e024b66
Jared Pérez <cacahuate.1895@gmail.com>
2023-11-18 02:11:57 -0600

    Footer

---
+++ BienesRaices/resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<!--Inicio footer-->
+<footer class="footer seccion">
+    <div class="contenedor contenedor-footer">
+        <nav class="navegacion">
+            <a href="nosotros.html">Nosotros</a>
+            <a href="anuncios.html">Anuncios</a>
+            <a href="blog.html">Blog</a>
+            <a href="contacto.html">Contacto</a>
+        </nav>
+    </div>
+
+    <p class="copyright">Todos los derechos Reservados 2021 &copy;</p>
+</footer>
+<!--Fin del footer-->
