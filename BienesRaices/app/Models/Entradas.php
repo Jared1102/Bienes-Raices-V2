532f38e49055f9b034e9778cec3c285094d517c6
prado <pradojimenezmaria@gmail.com>
2023-11-19 01:12:43 -0600

    Se agrego estructura y modelos

--- BienesRaices/app/Models/Entradas.php
+++ BienesRaices/app/Models/Entradas.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entradas extends Model
 {
+    public function rol()
+    {
+        return $this->belongsTo(User::class);
+    }
     use HasFactory;
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'imagen',
+        'user_id',
+    ];
 }
--- BienesRaices/resources/views/blog/index.blade.php
+++ BienesRaices/resources/views/blog/index.blade.php
@@ -4,4 +4,32 @@
     <!--
         Aqui se colocarian las entradas
     -->
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Nuestro Blog</h1>
+        @foreach ($entradas as $entrada)
+            <article class="entrada-blog">
+                <div class="imagen">
+                    <picture>
+                        {{-- <source srcset="build/img/blog1.webp" type="image/webp">
+                        <source srcset="build/img/blog1.jpg" type="image/jpeg"> --}}
+                        <img loading="lazy" src="{{Vite::asset('resources/img/blog1.jpg')}}" alt="Texto Entrada Blog">
+                    </picture>
+                </div>
+
+                <div class="texto-entrada">
+                    <a href="">
+                        <h4>Terraza en el techo de tu casa</h4>
+                        <p>Escrito el: <span>20/10/2023</span> por: <span>Admin</span> </p>
+
+                        <p>
+                            Consejos para construir una terraza en el techo de tu casa con los mejores materiales y ahorrando dinero
+                        </p>
+                    </a>
+                </div>
+            </article>
+        <!--Fin entrada-->
+        @endforeach
+        <!--Entrada-->
+        
+    </main>
 @endsection
