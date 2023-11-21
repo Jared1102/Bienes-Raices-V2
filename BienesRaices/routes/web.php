203ed1a184e27bfb56f8b2ae5096cab7820d18a1
Jared Pérez <cacahuate.1895@gmail.com>
2023-11-20 18:39:29 -0600

    Creando middleware para la seguridad por URL

--- BienesRaices/routes/web.php
+++ BienesRaices/routes/web.php
@@ -31,23 +31,34 @@ Route::get('/contacto',function(){
     return view('contacto.index');
 })->name('contacto');
 
+
+Route::middleware(['auth','admin'])->group(function(){
+    //Blog
+    Route::get('/blog/create',[EntradasController::class,'create'])->name('BlogCreate');
+    Route::post('/blog',[EntradasController::class,'store'])->name('BlogStore');
+    Route::get('/blog/{blog}/edit', [EntradasController::class,'edit'])->name('BlogEdit');
+    Route::patch('/blog/{blog}', [EntradasController::class,'update'])->name('BlogUpdate');
+    Route::delete('/blog/{blog}', [EntradasController::class,'destroy'])->name('BlogDestroy');
+
+    //Anuncios
+    Route::get('/anuncios/create',[AnunciosController::class,'create'])->name('AnunciosCreate');
+    Route::post('/anuncios',[AnunciosController::class,'store'])->name('AnunciosStore');
+    Route::get('/anuncios/{anuncio}/edit',[AnunciosController::class,'edit'])->name('AnunciosEdit');
+    Route::patch('/anuncios/{anuncio}',[AnunciosController::class,'update'])->name('AnunciosUpdate');
+    Route::delete('/anuncios/{anuncio}',[AnunciosController::class,'destroy'])->name('AnunciosDestroy');
+
+    
+});
+
 //Blog
 Route::get('/blog', [EntradasController::class,'index'])->name('indexblog');
 Route::get('/blog/{blog}/show',[EntradasController::class,'show'])->name('BlogShow');
-Route::get('/blog/create',[EntradasController::class,'create'])->name('BlogCreate');
-Route::post('/blog',[EntradasController::class,'store'])->name('BlogStore');
-Route::get('/blog/{blog}/edit', [EntradasController::class,'edit'])->name('BlogEdit');
-Route::patch('/blog/{blog}', [EntradasController::class,'update'])->name('BlogUpdate');
-Route::delete('/blog/{blog}', [EntradasController::class,'destroy'])->name('BlogDestroy');
+
 
 //Anuncios
 Route::get('/anuncios',[AnunciosController::class,'index'])->name('AnunciosIndex');
 Route::get('/anuncios/{anuncio}/show',[AnunciosController::class,'show'])->name('AnuncioShow');
-Route::get('/anuncios/create',[AnunciosController::class,'create'])->name('AnunciosCreate');
-Route::post('/anuncios',[AnunciosController::class,'store'])->name('AnunciosStore');
-Route::get('/anuncios/{anuncio}/edit',[AnunciosController::class,'edit'])->name('AnunciosEdit');
-Route::patch('/anuncios/{anuncio}',[AnunciosController::class,'update'])->name('AnunciosUpdate');
-Route::delete('/anuncios/{anuncio}',[AnunciosController::class,'destroy'])->name('AnunciosDestroy');
+
 
 //Auth
 Route::get('/login',[UserController::class,'indexLogin'])->name('IndexLogin');
