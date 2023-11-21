f49ac60c7771c026835270f0667eea972b0a19c6
prado <pradojimenezmaria@gmail.com>
2023-11-21 00:01:56 -0600

    Se termino de crear el login

--- BienesRaices/resources/views/layouts/app_login.blade.php
+++ BienesRaices/resources/views/layouts/app_login.blade.php
@@ -8,13 +8,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
+        <script src="https://kit.fontawesome.com/b4fe636f2d.js" crossorigin="anonymous"></script>
+
         @vite(['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body>
-        @include('partials.header_general')
+    <body style="background: url({{Vite::asset('resources/img/header.jpg')}})">
+        @include('partials.header_login')
         @yield('container')
-        @include('partials.footer')      
+        {{-- @include('partials.footer')       --}}
         @yield('js') 
     </body>
 </html>
